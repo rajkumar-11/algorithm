3b31b0151a7e899e0af957369ba2627561c72dd7
rajkumar <rajkumarnavhaal@gmail.com>
2019-07-30 19:15:34 +0530

    new problems solved

---
+++ src/CheckGit.java
@@ -0,0 +1,6 @@
+public class CheckGit {
+    public static void main(String[] args) {
+        System.out.println("pushed in git");
+    }
+
+}
--- src/Practice.java
+++ src/Practice.java
@@ -6,7 +6,7 @@ public class Practice {
 	
 		Graph11 graph= new Graph11(5);
 		
-		grpah.
+		//grpah.
 		
 		
 		
