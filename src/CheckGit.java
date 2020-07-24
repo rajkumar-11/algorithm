8ab1fb58dfc058fab03bd087af5b561a1b26b21a
rajkumar <rajkumarnavhaal@gmail.com>
2020-07-24 10:07:51 +0530

    new problems solved

--- src/CheckGit.java
+++ src/CheckGit.java
@@ -2,6 +2,7 @@ public class CheckGit {
     public static void main(String[] args) {
         System.out.println("pushed in git");
         System.out.println("starting the new phase");
+        System.out.println("starting on 24th of june");
     }
 
 }
